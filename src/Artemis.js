ac898c9f47c5a310392fbd2e027679f30952323c
cojack <xcojack@gmail.com>
2013-06-24 14:47:06 -0400

    create additional namespaces for ArtemiJS behaviur

--- src/Artemis.js
+++ src/Artemis.js
@@ -8,5 +8,10 @@
         license: 'GPLv2',
         env: 1 // 1 - dev, 2 - test, 4 - prod
     };
+    
+    ArtemiJS.Managers = {};
+    ArtemiJS.Systems = {};
+    ArtemiJS.Utils = {};
+    
     window.ArtemiJS = ArtemiJS;
 })(window);
