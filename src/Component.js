ebe631123f837270c2c3ffd381762f6198746f89
cojack <xcojack@gmail.com>
2013-06-26 07:57:35 -0400

    add docs for Component

--- src/Component.js
+++ src/Component.js
@@ -1,9 +1,14 @@
 (function(ArtemiJS) {
     'use strict';
     
-    var Component = function() {
-        
-    };
+    /**
+     * A tag class. All components in the system must extend this class.
+     * 
+     * @module ArtemiJS
+     * @class Component
+     * @constructor
+     */
+    var Component = function() {};
 
     ArtemiJS.Component = Component;
 })(window.ArtemiJS || {});
