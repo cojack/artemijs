7ab31205dff49c3d5338c43ffe9cda7c0bcd768b
cojack <xcojack@gmail.com>
2013-06-27 16:29:35 -0400

    rename Artemis.js to Artemi.js

--- src/Artemi.js
+++ src/Artemi.js
@@ -2,10 +2,34 @@
     'use strict';
     
     // this file have to be included first in yuicompressor
+    
+    /**
+     * Entity Framework
+     * 
+     * @module ArtemiJS
+     * @class ArtemiJS
+     * @main ArtemiJS
+     */
     var ArtemiJS = {
+        
+        /**
+         * @property {Float} version
+         */
         version: 0.1,
+        
+        /**
+         * @property {String} source
+         */
         source: 'https://github.com/cojack/artemijs',
+        
+        /**
+         * @property {String} license
+         */
         license: 'GPLv2',
+        
+        /**
+         * @property {Number} env
+         */
         env: 1 // 1 - dev, 2 - test, 4 - prod
     };
     
