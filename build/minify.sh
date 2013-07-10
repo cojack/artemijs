989733bfe69307503b9532f7f3d96d52b75f1e86
cojack <xcojack@gmail.com>
2013-07-10 14:50:16 -0400

    change buildset for Artemi.js

--- build/minify.sh
+++ build/minify.sh
@@ -3,10 +3,12 @@
 # path to the yui compressor
 YUICOMP_PATH="";
 
+mkdir ./../lib/
+
 cat ../utils/Array.js \
     ../utils/Math.js \
     ../utils/Object.js \
-    ../Artemis.js \
+    ../Artemi.js \
     ../utils/Assert.js \
     ../utils/Bag.js \
     ../utils/BitSet.js \
@@ -19,10 +21,10 @@ cat ../utils/Array.js \
     ../Entity.js \
     ../EntityManager.js \
     ../EntitySystem.js \
-    ../World.js > artemijs.dev.js
+    ../World.js > ./../lib/artemijs.dev.js
 
-sed -i 's/env: 1/env: 2/g' artemijs.dev.js
-java -jar $YUICOMP_PATH --type js --charset utf-8 --nomunge --disable-optimizations -o artemijs.test.js $(pwd)/artemijs.dev.js
-sed -i 's/env: 2/env: 4/g' artemijs.dev.js
-java -jar $YUICOMP_PATH --type js --charset utf-8 -o artemijs.min.js $(pwd)/artemijs.dev.js
-sed -i 's/env: 4/env: 1/g' artemijs.dev.js
+sed -i 's/env: 1/env: 2/g' ./../lib/artemijs.dev.js
+java -jar $YUICOMP_PATH --type js --charset utf-8 --nomunge --disable-optimizations -o ./../lib/artemijs.test.js ./../lib/artemijs.dev.js
+sed -i 's/env: 2/env: 4/g' ./../lib/artemijs.dev.js
+java -jar $YUICOMP_PATH --type js --charset utf-8 -o ./../lib/artemijs.min.js ./../lib/artemijs.dev.js
+sed -i 's/env: 4/env: 1/g' ./../lib/artemijs.dev.js
