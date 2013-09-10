90a3a37bee959b7a9e309d448895b952ecf6cd9e
cojack <xcojack@gmail.com>
2013-09-10 15:27:09 -0400

    use browserify to accomplish cross system compatibility

--- build/minify.sh
+++ build/minify.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
+MY_PATH=$(dirname "$0")
+
 # path to the yui compressor
-YUICOMP_PATH="";
+YUICOMP_PATH="$MY_PATH/yuicompressor-2.4.7.jar";
 
-mkdir ./../lib/
 
-cat ../utils/Array.js \
-    ../utils/Math.js \
-    ../utils/Object.js \
-    ../Artemi.js \
-    ../utils/Assert.js \
-    ../utils/Bag.js \
-    ../utils/BitSet.js \
-    ../Aspect.js \
-    ../Manager.js \
-    ../Component.js \
-    ../ComponentManager.js \
-    ../ComponentMapper.js \
-    ../EntityObserver.js \
-    ../Entity.js \
-    ../EntityManager.js \
-    ../EntitySystem.js \
-    ../World.js > ./../lib/artemijs.dev.js
+if [ ! -d "$MY_PATH/../lib/" ]; then
+    $(mkdir "$MY_PATH/../lib/")
+fi
 
-sed -i 's/env: 1/env: 2/g' ./../lib/artemijs.dev.js
-java -jar $YUICOMP_PATH --type js --charset utf-8 --nomunge --disable-optimizations -o ./../lib/artemijs.test.js ./../lib/artemijs.dev.js
-sed -i 's/env: 2/env: 4/g' ./../lib/artemijs.dev.js
-java -jar $YUICOMP_PATH --type js --charset utf-8 -o ./../lib/artemijs.min.js ./../lib/artemijs.dev.js
-sed -i 's/env: 4/env: 1/g' ./../lib/artemijs.dev.js
+cat "$MY_PATH/../src/native/Array.js" \
+    "$MY_PATH/../src/native/Math.js" \
+    "$MY_PATH/../src/native/Object.js" > "$MY_PATH/../lib/artemijs.dev.js"
+    
+browserify "$MY_PATH/../src/Artemi.js" >> "$MY_PATH/../lib/artemijs.dev.js" -s ArtemiJS -d
+
+sed -i 's/env: 1/env: 2/g' "$MY_PATH/../lib/artemijs.dev.js"
+java -jar $YUICOMP_PATH --type js --charset utf-8 --nomunge --disable-optimizations -o "$MY_PATH/../lib/artemijs.test.js" "$MY_PATH/../lib/artemijs.dev.js"
+sed -i 's/env: 2/env: 4/g' "$MY_PATH/../lib/artemijs.dev.js"
+java -jar $YUICOMP_PATH --type js --charset utf-8 -o "$MY_PATH/../lib/artemijs.min.js" "$MY_PATH/../lib/artemijs.dev.js"
+sed -i 's/env: 4/env: 1/g' "$MY_PATH/../lib/artemijs.dev.js"
