7c053b4f1ec488f60f56d0b7780854400b24f50c
cojack <xcojack@gmail.com>
2020-06-23 09:49:16 +0200

    feat(src): rewrite component-type to typescript

---
+++ src/component-type.ts
@@ -0,0 +1,36 @@
+import {Component} from './component';
+import {Constructor} from './utils';
+
+export class ComponentType {
+
+	private static INDEX = 0;
+
+	private readonly index: number;
+
+	private constructor(private readonly type: Constructor<Component>) {
+		this.index = ComponentType.INDEX++;
+	}
+
+	public getIndex(): number {
+		return this.index;
+	}
+
+	public toString(): string {
+		return 'ComponentType[' + this.type.prototype.name + '] (' + this.index + ')';
+	}
+
+	private static componentTypes = new Map<Constructor<Component>, ComponentType>();
+
+	public static getTypeFor<T extends Component>(component: Constructor<T>): ComponentType {
+		let type = this.componentTypes.get(component);
+		if (!type) {
+			type = new ComponentType(component);
+			ComponentType.componentTypes.set(component, type);
+		}
+		return type;
+  }
+
+	public static getIndexFor<T extends Component>(component: Constructor<T>): number {
+		return ComponentType.getTypeFor(component).getIndex();
+	}
+}
