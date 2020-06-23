0613c41161b4169135cdfb0054972b2dbc204eee
cojack <xcojack@gmail.com>
2020-06-23 20:07:27 +0200

    misc(*): summary

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export * from './utils';
+export * from './managers';
+
+export * from './aspect';
+export * from './component';
+export * from './component-manager';
+export * from './component-mapper';
+export * from './component-type';
+export * from './entity';
+export * from './entity-manager';
+export * from './entity-observer';
+export * from './entity-system';
+export * from './manager';
+export * from './world';
